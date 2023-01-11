5f8be7ea1b9a9232c790e62eee8ce9840462833a
Jonathan Ismael Zapata Sandoval <84453014+JonathanZapata1@users.noreply.github.com>
2023-01-11 02:28:23 -0500

    Complemento

--- Code/Ecuabonsai/client/src/Vistas/UserVistas/Contacto.js
+++ Code/Ecuabonsai/client/src/Vistas/UserVistas/Contacto.js
@@ -1,11 +1,30 @@
 import { NavbarUser } from "../../Components/UserForm/NavbarUser";
+import { Footer } from "../../Components/UserForm/Footer";
+import "./user.css"
+import Foto1 from '../../Assets/bonsai1.png'
+import Foto2 from '../../Assets/bonsai2.png'
+import Foto3 from '../../Assets/bonsai3.png'
 export function Contacto() {
   return (
     <div >
     <NavbarUser />
-      <div className="form">
-       
+    <section className="menu">
+      <h1> ESCRÍBENOS</h1>
+    <div className="columna">
+      <div className="foto">
+  <img src={Foto1} />
+      </div>
+      <div className="foto">
+        <img src={Foto2} />
+      </div>
+      <div className="foto">
+        <img src={Foto3} />
       </div>
     </div>
+  </section>
+  <Footer />
+    </div>
+    
+    
   );
 }
--- Code/Ecuabonsai/client/src/Vistas/UserVistas/Mantenimiento.js
+++ Code/Ecuabonsai/client/src/Vistas/UserVistas/Mantenimiento.js
@@ -1,11 +1,44 @@
 import { NavbarUser } from "../../Components/UserForm/NavbarUser";
+import { Footer } from "../../Components/UserForm/Footer";
 export function Mantenimiento() {
   return (
     <div >
     <NavbarUser />
-      <div className="form">
-       
+      <div className="form2">
+      <div className="mensaje">
+          </div>
+        <section className="principio">
+          <h1> Objetivos: </h1>
+          <div className="oracion">
+            <div>
+              <i className="fas fa-hamburger" />
+              <span>
+                Entrega del producto en buen estado y con un excelente actitud. 
+              </span> 
+            </div>
+            <div>
+              <i className="fas fa-hamburger" />
+              <span>
+                Firmes defensores de que calidad y belleza no está en el precio, sino en el producto.
+              </span> 
+            </div>
+            <div>
+              <i className="fas fa-hamburger" />
+              <span>
+                Exigentes en el cuidado del arboles bonsai.
+              </span> 
+            </div>
+            <div>
+              <i className="fas fa-hamburger" />
+              <span>
+                Llevar un ambiente moderno y clasico a tu hogar. 
+              </span> 
+            </div>
+          </div>
+        </section>
       </div>
+      <Footer />
     </div>
+    
   );
 }
