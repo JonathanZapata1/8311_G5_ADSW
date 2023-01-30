b81fb6dfc54dc6b2a89d9b752de05bdc9f06335e
Bryan Sebastián Yanzapanta Ontaneda <bsyanzapanta@espe.edu.ec>
2023-01-29 19:12:30 -0500

    metodo post registro en progreso

--- Code/Ecuabonsai/client/src/Components/Login.js/Register.js
+++ Code/Ecuabonsai/client/src/Components/Login.js/Register.js
@@ -1,10 +1,43 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 import "./Login.css";
-import { validarLogin } from "../../Controller/loginController";
+/*import { validarLogin } from "../../Controller/loginController";*/
 
 export function Register() {
-  let [username, setUsername] = useState("");
-  let [password, setPassword] = useState("");
+  const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleNameChange = (event) => {
+      setName(event.target.value);
+    };
+    const handleUsernameChange = (event) => {
+      setUsername(event.target.value);
+    };
+    const handleEmailChange = (event) => {
+      setEmail(event.target.value);
+    };
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+    };
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const response = await axios.post('http://localhost:5000/user', {
+          name,
+          username,
+          email,
+          password,
+        });
+  
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
   return (
     <div className="containerlogin">
